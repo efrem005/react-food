684234068f552674f1411bcb620d5afe87f155f8
Nikolay Efremov <efrem_nn@mail.ru>
2023-10-16 10:02:07 +0700

    delete and pro files

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import "materialize-css/dist/css/materialize.min.css";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home.jsx";
 import { About } from "./pages/About.jsx";
 import { Contact } from "./pages/Contact.jsx";
 import { NotFound } from "./pages/NotFound.jsx";
 import { Category } from "./pages/Category.jsx";
 import { Layout } from "./layout/layout.jsx";
-import { Login } from "./pages/Login.jsx";
-import { Auth } from "./hoc/Auth.jsx";
 import { Recipe } from "./pages/Recipe.jsx";
 
 function App() {
 
     return (
-        <BrowserRouter basename='/react-food'>
+        <BrowserRouter basename='/react-food/'>
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { AuthProvider } from "./hoc/AuthProvider.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
